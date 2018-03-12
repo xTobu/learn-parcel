15c1dcc69fd64060d00113157f7664cc9c572fbc
Junxiang <junxiang.oriint@gmail.com>
2018-03-12 12:08:17 +0800

    add jest

---
+++ js/add.js
@@ -0,0 +1,9 @@
+function add(a, b) {
+  console.log(a + b);
+}
+function add2(a, b) {
+    console.log(a - b);
+  }
+
+module.exports.add = add;
+module.exports.add2 = add2;
---
+++ js/entry.js
@@ -0,0 +1,4 @@
+import { add, add2 } from "./add";
+
+add(1, 5);
+add2(1, 5);
